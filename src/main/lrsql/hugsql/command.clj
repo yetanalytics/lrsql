8e1001b041786ee89d0382f31f3f70e667614c8e
kelvinqian00 <kelvinqian2@gmail.com>
2021-05-19 10:02:57 -0400

    Revise throw-invalid-table-ex

--- src/main/lrsql/hugsql/command.clj
+++ src/main/lrsql/hugsql/command.clj
@@ -18,16 +18,21 @@
 
 (defmacro throw-invalid-table-ex
   "Throw an exception with the following error data:
-   
-   :kind  ::invalid-table
-   :table <table name>
-   :input <input>"
+     :kind     ::invalid-table
+     :table    <table name>
+     :input    `input`
+     :fn-name  `fn-name`"
   [fn-name input]
-  `(throw
-    (ex-info (format "`%s` is not supported for this table type" ~fn-name)
-             {:kind  ::invalid-table
-              :table ~(:table input)
-              :input ~input})))
+  (let [table-kw#   (:table input)
+        table-name# (when table-kw# (name table-kw#))]
+    `(throw
+      (ex-info (format "`%s` is not supported for table type `%s`"
+                       ~fn-name
+                       ~table-name#)
+               {:kind    ::invalid-table
+                :table   ~table-kw#
+                :input   ~input
+                :fn-name ~fn-name}))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Insertions
