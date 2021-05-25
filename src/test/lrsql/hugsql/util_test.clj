7f614c6a53342c48ab2505dc98c12eba3a65f26d
kelvinqian00 <kelvinqian2@gmail.com>
2021-05-25 16:58:14 -0400

    Add lex sort test

--- src/test/lrsql/hugsql/util_test.clj
+++ src/test/lrsql/hugsql/util_test.clj
@@ -1,6 +1,7 @@
 (ns lrsql.hugsql.util-test
   (:require [clojure.test :refer [deftest testing is]]
             [clj-uuid]
+            [xapi-schema.spec.regex :as xsr]
             [lrsql.test-support :refer [check-validate]]
             [lrsql.hugsql.util :as util]))
 
@@ -10,7 +11,7 @@
     (is (nil? (check-validate `util/generate-squuid))))
   (testing "squuid monotonicity"
     (is
-     (loop [squuid-seq  (repeatedly 1000 util/generate-squuid)
+     (loop [squuid-seq  (repeatedly 10000 util/generate-squuid)
             squuid-seq' (rest squuid-seq)]
        (if (empty? squuid-seq')
          true
@@ -18,4 +19,10 @@
                next-squuid (first squuid-seq')]
            (if (clj-uuid/uuid< prev-squuid next-squuid)
              (recur squuid-seq' (rest squuid-seq'))
-             false)))))))
+             false))))))
+  (testing "squuid monotonicity (lex sort)"
+    (let [squuid-seq (->> (repeatedly 1000 util/generate-squuid)
+                          (map util/uuid->str))
+          squuid-seq' (sort squuid-seq)]
+      (is (every? (partial re-matches xsr/UuidRegEx) squuid-seq'))
+      (is (= squuid-seq squuid-seq')))))
