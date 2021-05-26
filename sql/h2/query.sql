0669355b1232b0c165caf79cb2af0c178d8ce1de
kelvinqian00 <kelvinqian2@gmail.com>
2021-05-26 10:48:16 -0400

    Exclude voided statements from queries

--- sql/h2/query.sql
+++ sql/h2/query.sql
@@ -22,15 +22,15 @@ SELECT xapi_statement.id, payload FROM xapi_statement
        "\nAND statement_to_activity.usage = 'Object'")))
 ~*/
 WHERE 1
---~ (when (:statement-id params)    "AND xapi_statement.statement_id = :statement-id")
---~ (when (:from params)            "AND xapi_statement.id >= :from")
---~ (when (some? (:voided? params)) "AND is_voided = :voided?")
---~ (when (:verb-iri params)        "AND verb_iri = :verb-iri")
---~ (when (:registration params)    "AND registration = :registration")
---~ (when (:since params)           "AND stored > :since")
---~ (when (:until params)           "AND stored <= :until")
---~ (when (:ascending? params)      "ORDER BY stored")
---~ (when (:limit params)           "LIMIT :limit")
+--~ (if (some? (:voided? params)) "AND is_voided = :voided?" "AND is_voided = FALSE")
+--~ (when (:statement-id params)  "AND xapi_statement.statement_id = :statement-id")
+--~ (when (:from params)          "AND xapi_statement.id >= :from")
+--~ (when (:verb-iri params)      "AND verb_iri = :verb-iri")
+--~ (when (:registration params)  "AND registration = :registration")
+--~ (when (:since params)         "AND stored > :since")
+--~ (when (:until params)         "AND stored <= :until")
+--~ (when (:ascending? params)    "ORDER BY stored")
+--~ (when (:limit params)         "LIMIT :limit")
 
 /* Statement Object Queries */
 
