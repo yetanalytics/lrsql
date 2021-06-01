75c4f2c2d1a4802d8e4170dd13df08aefc77d899
Milton Reder <milt@yetanalytics.com>
2021-06-01 12:21:00 -0400

    Improved conformance testing (#24)
    * updated test runner to quieter version
    
    * Mine test results for conformance req. codes and enumerate in a test
    
    * More test assertions
    
    * Remove count test
    
    * remove err count
    
    * glorious annotation of remaining known failures
    
    * Run a fresh LRS for each test
    
    * latest runner with dynamic reporter fn
    
    * Add regression test and handle stateful failures
    
    * annotation for mysterious XAPI-00164
    
    * enable printing of output for regressions

--- src/test/lrsql/test_support.clj
+++ src/test/lrsql/test_support.clj
@@ -1,6 +1,7 @@
 (ns lrsql.test-support
   (:require [config.core :refer [env]]
-            [clojure.spec.test.alpha :as stest])
+            [clojure.spec.test.alpha :as stest]
+            [clojure.string :as cs])
   (:import [java.util UUID]))
 
 (defn fresh-db-fixture
@@ -33,3 +34,50 @@
          res (stest/check fname opts)]
      (when-not (true? (-> res first :clojure.spec.test.check/ret :pass?))
        res))))
+
+(defn tests-seq
+  "Given nested xapi conformance logs, flatten them into a seq"
+  [logs]
+  (mapcat
+   (fn splode [{:keys [title
+                       status
+                       tests]
+                :as test}
+               & {:keys [depth]
+                  :or {depth 0}}]
+     (cons (-> test
+               (assoc :depth depth))
+           (mapcat #(splode % :depth (inc depth))
+                   tests)))
+   (map :log logs)))
+
+(defn test-codes
+  "Extract codes from a test"
+  [{:keys [title name requirement]}]
+  (re-seq
+   #"XAPI-\d\d\d\d\d"
+   (str
+    title
+    name
+    (if (coll? requirement)
+      (apply str requirement)
+      requirement))))
+
+(defn req-code-set
+  "Return a set of mentioned XAPI-XXXXX codes in test results"
+  [tests]
+  (into #{}
+        (mapcat
+         test-codes
+         tests)))
+
+(defn filter-code
+  "Filter tests with the given code"
+  [code tests]
+  (filter
+   #(contains?
+     (into #{}
+           (test-codes
+            %))
+     code)
+   tests))
