6d9d5c43f62b29d23c7dbc411025340fd306c997
kelvinqian00 <kelvinqian2@gmail.com>
2021-06-01 15:21:10 -0400

    Nuke user.clj

--- src/dev/lrsql/user.clj
+++ src/dev/lrsql/user.clj
@@ -4,32 +4,16 @@
             [lrsql.system :as system]))
 
 (comment
-  (require '[lrsql.lrs-test :refer [stmt-0 stmt-1 stmt-2]]
-           '[next.jdbc :as jdbc]
-           '[com.yetanalytics.lrs.protocol :as lrsp])
+  (require '[next.jdbc :as jdbc]
+           '[com.yetanalytics.lrs.protocol :as lrsp]
+           '[lrsql.hugsql.util.statement :as u])
 
   (def sys (system/system))
   (def sys' (component/start sys))
 
   (def lrs (:lrs sys'))
   (def ds ((-> sys' :lrs :conn-pool)))
-
-  (lrsp/-store-statements lrs {} [stmt-0 stmt-1 stmt-2] [])
-
-;; LEFT JOIN xapi_statement stmt_desc
-;; ON stmt_desc.statement_id = statement_to_statement.descendant_id
-
-  ;; LEFT JOIN statement_to_activity stmt_desc_activ
-;; ON stmt_desc.statement_id = stmt_desc_activ.statement_id
-
-  (jdbc/execute! ds ["SELECT *
-                      FROM xapi_statement stmt
-                      LEFT JOIN statement_to_statement
-                      ON stmt.statement_id = statement_to_statement.ancestor_id
-                      "])
-
-  (jdbc/execute! ds ["SELECT * FROM statement_to_statement"])
-
+  
   (doseq [cmd [;; Drop document tables
                "DROP TABLE IF EXISTS state_document"
                "DROP TABLE IF EXISTS agent_profile_document"
