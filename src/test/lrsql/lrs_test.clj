f9b4655f8ef838c52fee996879a37ce6c58b21f7
Milt Reder <milt@yetanalytics.com>
2021-06-08 17:13:08 -0400

    use fixture in lrs tests again

--- src/test/lrsql/lrs_test.clj
+++ src/test/lrsql/lrs_test.clj
@@ -84,24 +84,6 @@
    :length      27
    :sha2        "495395e777cd98da653df9615d09c0fd6bb2f8d4788394cd53c56a3bfdcd848a"})
 
-(defn- drop-all!
-  "Drop all tables in the db, in preparation for adding them again.
-   DO NOT RUN THIS DURING PRODUCTION!!!"
-  [tx]
-  (doseq [cmd [;; Drop document tables
-               "DROP TABLE IF EXISTS state_document"
-               "DROP TABLE IF EXISTS agent_profile_document"
-               "DROP TABLE IF EXISTS activity_profile_document"
-               ;; Drop statement tables
-               "DROP TABLE IF EXISTS statement_to_statement"
-               "DROP TABLE IF EXISTS statement_to_activity"
-               "DROP TABLE IF EXISTS statement_to_actor"
-               "DROP TABLE IF EXISTS attachment"
-               "DROP TABLE IF EXISTS activity"
-               "DROP TABLE IF EXISTS actor"
-               "DROP TABLE IF EXISTS xapi_statement"]]
-    (jdbc/execute! tx [cmd])))
-
 (defn- remove-props
   "Remove properties added by `input/prepare-statement`."
   [statement]
@@ -111,7 +93,7 @@
       (dissoc "authority")
       (dissoc "version")))
 
-#_(use-fixtures :each support/fresh-db-fixture)
+(use-fixtures :each support/fresh-db-fixture)
 
 (deftest test-statement-fns
   (let [_     (support/assert-in-mem-db)
@@ -289,8 +271,6 @@
                          "definition" {"name"        {"en-US" "Multi Part Activity"}
                                        "description" {"en-US" "Multi Part Activity Description"}}}}
              (lrsp/-get-activity lrs {} {:activityId act-1}))))
-    (jdbc/with-transaction [tx (-> lrs :connection :conn-pool)]
-      (drop-all! tx))
     (component/stop sys')))
 
 (def stmt-1'
@@ -382,8 +362,6 @@
                  (update-in [:statement-result :statements]
                             (partial map remove-props))
                  (update :statement-result dissoc :more)))))
-    (jdbc/with-transaction [tx (-> lrs :connection :conn-pool)]
-      (drop-all! tx))
     (component/stop sys')))
 
 (def state-id-params
@@ -512,6 +490,4 @@
       (is (nil? (lrsp/-get-document lrs
                                     {}
                                     activity-prof-id-params))))
-    (jdbc/with-transaction [tx (-> lrs :connection :conn-pool)]
-      (drop-all! tx))
     (component/stop sys')))
