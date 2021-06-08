1f919d8acc962ac26a53c75bfba0de3772a8ad36
Milt Reder <milt@yetanalytics.com>
2021-06-08 17:03:41 -0400

    additional fixture tests to debug this

--- src/test/lrsql/test_support_test.clj
+++ src/test/lrsql/test_support_test.clj
@@ -1,20 +1,37 @@
 (ns lrsql.test-support-test
   (:require [aero.core :refer [read-config]]
             [lrsql.test-support :refer :all]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all]
+            [lrsql.system :as system]))
 
 (defn- get-db-name
   [config]
   (get-in config [:database :db-name]))
 
 (deftest fresh-db-fixture-test
-  (is
-   (not=
-    (get-db-name
-     (fresh-db-fixture
-      #(read-config "config.edn"
-                    {:profile :test})))
-    (get-db-name
-     (fresh-db-fixture
-      #(read-config "config.edn"
-                    {:profile :test}))))))
+  (testing "sets the db name to a random uuid"
+    (is
+     (uuid?
+      (java.util.UUID/fromString
+       (get-db-name
+        (fresh-db-fixture
+         #(read-config "config.edn"
+                       {:profile :test})))))))
+  (testing "changes db-name"
+    (is
+     (not=
+      (get-db-name
+       (fresh-db-fixture
+        #(read-config "config.edn"
+                      {:profile :test})))
+      (get-db-name
+       (fresh-db-fixture
+        #(read-config "config.edn"
+                      {:profile :test}))))))
+  (testing "sets system db-name"
+    (is
+     (not= "example"
+           (get-in
+            (fresh-db-fixture
+             #(system/system))
+            [:database :db-name])))))
