8ad64b866b7cd5203229c8fc98f09b734916ddf9
kelvinqian00 <kelvinqian2@gmail.com>
2021-07-07 12:27:23 -0400

    Update logging

--- src/main/lrsql/system/webserver.clj
+++ src/main/lrsql/system/webserver.clj
@@ -91,10 +91,20 @@
                          i/xapi-default-interceptors
                          http/create-server
                          http/start)]
-         (log/infof "Starting new webserver at host %s and port %s"
-                    (::http/host service)
-                    (::http/port service))
+         ;; Logging
+         (let [{{ssl-port :ssl-port} ::http/container-options
+                http-port ::http/port
+                host ::http/host} service]
+           (if http-port
+             (log/infof "Starting new webserver at host %s, HTTP port %s, and SSL port %s"
+                        host
+                        http-port
+                        ssl-port)
+             (log/infof "Starting new webserver at host %s and SSL port %s"
+                        host
+                        ssl-port)))
          (log/tracef "Server map: %s" server)
+         ;; Return new webserver
          (assoc this
                 :service service
                 :server server))
