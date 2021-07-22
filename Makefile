c25276009121bdb9cea156fb09428f4b02305f19
kelvinqian00 <kelvinqian2@gmail.com>
2021-07-22 10:42:01 -0400

    Don't overengineer main arg parsing

--- Makefile
+++ Makefile
@@ -18,10 +18,10 @@ ephemeral: keystore
 	LRSQL_DB_NAME=ephemeral \
 		LRSQL_SEED_API_KEY=username \
 		LRSQL_SEED_API_SECRET=password \
-		clojure -M:db-h2 -m lrsql.h2.main -p false
+		clojure -M:db-h2 -m lrsql.h2.main --persistent false
 
 persistent: keystore
 	LRSQL_DB_NAME=persistent \
 		LRSQL_SEED_API_KEY=username \
 		LRSQL_SEED_API_SECRET=password \
-		clojure -M:db-h2 -m lrsql.h2.main -p true
+		clojure -M:db-h2 -m lrsql.h2.main --persistent true
--- src/h2/lrsql/h2/main.clj
+++ src/h2/lrsql/h2/main.clj
@@ -1,37 +1,18 @@
 (ns lrsql.h2.main
-  (:require [clojure.string :refer [lower-case]]
-            [com.stuartsierra.component :as component]
+  (:require [com.stuartsierra.component :as component]
             [lrsql.system :as system]
             [lrsql.h2.record :as ir])
   (:gen-class))
 
 (def h2-interface (ir/map->H2Interface {}))
 
-(defn- throw-bad-args [args]
-  (throw (IllegalArgumentException.
-          (format "Illegal main args: %s" (str args)))))
-
-(defn- parse-persistent
-  "Parse the values of the flags `-p` and `--persistent`. Throws an error
-   if both are present and have different vals, or if the val is invalid."
-  [?short-per ?long-per]
-  (if (and (and ?short-per ?long-per)
-           (not= (lower-case ?short-per) (lower-case ?long-per)))
-    (throw-bad-args (format "-p %s, --persistent %s" ?short-per ?long-per))
-    (let [per (or ?short-per ?long-per)]
-      (case (lower-case per)
-        "true"  true
-        "false" false
-        nil     false
-        :else   (throw-bad-args per)))))
-
 (defn -main
-  "Main entrypoint for H2-backed LRSQL instances. Passing `-p true` or
-   `--persistent true` will spin up a persistent H2 instance on disk;
-   otherwise, an in-mem, ephemeral H2 instance will init instead."
+  "Main entrypoint for H2-backed LRSQL instances. Passing `--persistent true`
+   will spin up a persistent H2 instance on disk; otherwise, an in-mem,
+   ephemeral H2 instance will init instead."
   [& args]
-  (let [{?short-per "-p" ?long-per "--persistent"} args
-        persistent? (parse-persistent ?short-per ?long-per)
+  (let [{?per-str "--persistent"} args
+        persistent? (Boolean/parseBoolean ?per-str)
         profile     (if persistent? :prod-h2 :prod-h2-mem)]
     (-> (system/system h2-interface profile)
         component/start)))
