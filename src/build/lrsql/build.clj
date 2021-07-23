6fa07b459c9ccb0107223ae8a970bfd56840cb27
Milt Reder <milt@yetanalytics.com>
2021-07-23 14:38:32 -0400

    Added manual threaded example that makes the same artifact

--- src/build/lrsql/build.clj
+++ src/build/lrsql/build.clj
@@ -2,18 +2,31 @@
   "Build utils for LRSQL artifacts"
   (:require [hf.depstar :as depstar]))
 
+(def uber-params
+  {:jar "target/bundle/lrsql.jar"
+   :aot true
+   :aliases [:db-h2 :db-sqlite]
+   :compile-ns :all
+   :no-pom true
+   :exclude ["^lrsql.*clj$"
+             "^.*sql$"
+             "^.*yetanalytics.*clj$"
+             ;; don't ship keystores
+             "^.*jks$"]})
+
 (defn uber
   "All backends, as an uberjar"
   [params]
-  (-> {:jar "target/bundle/lrsql.jar"
-       :aot true
-       :aliases [:db-h2 :db-sqlite]
-       :compile-ns :all
-       :no-pom true
-       :exclude ["^lrsql.*clj$"
-                 "^.*sql$"
-                 "^.*yetanalytics.*clj$"
-                 ;; don't ship keystores
-                 "^.*jks$"]}
+  (-> uber-params
       (merge params)
       (depstar/uberjar)))
+
+(defn uber-manual
+  "All backends, as an uberjar, manually"
+  [params]
+  (-> (merge uber-params
+             {:target-dir "target"})
+      (merge params)
+      (depstar/aot)
+      (assoc :jar-type :uber)
+      (depstar/build)))
