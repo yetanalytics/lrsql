db75d6d6ba9dcad35a07f8421d8c51a75cd9b25f
Milt Reder <milt@yetanalytics.com>
2021-07-29 10:14:11 -0400

    SQL-54 semi-clojure-align selfie

--- src/main/lrsql/util/cert.clj
+++ src/main/lrsql/util/cert.clj
@@ -32,28 +32,23 @@
   [& {:keys [^String dn
              ^Long days
              ^String alg]
-      :or {dn "CN=com.yetanalytics.lrsql, OU=Dev, O=Yet Analytics, L=Baltimore, S=Maryland, C=US"
-           days 365000 ;; 1000 years of darkness
-           alg "SHA256withRSA"}}]
+      :or   {dn   "CN=com.yetanalytics.lrsql, OU=Dev, O=Yet Analytics, L=Baltimore, S=Maryland, C=US"
+             days 365000 ;; 1000 years of darkness
+             alg  "SHA256withRSA"}}]
   (let [;; gen a pair
-        ^KeyPairGenerator kpg (KeyPairGenerator/getInstance "RSA")
-        ^KeyPair key-pair
-        (.generateKeyPair kpg)
-
-        ^PrivateKey private-key (.getPrivate key-pair)
-        ^java.util.Date from (new java.util.Date)
-        ^java.util.Date to (new java.util.Date
-                                (-> from
-                                    .getTime
-                                    (+ (* days 86400000))))
-        ^CertificateValidity interval (new CertificateValidity
-                                           from to)
-        ^BigInteger sn (new BigInteger
-                            64
-                            (new SecureRandom))
-        ^X500Name owner (new X500Name dn)
-        ^AlgorithmId algo (new AlgorithmId
-                               AlgorithmId/md5WithRSAEncryption_oid)
+        ^KeyPairGenerator kpg         (KeyPairGenerator/getInstance "RSA")
+        ^KeyPair key-pair             (.generateKeyPair kpg)
+        ^PrivateKey private-key       (.getPrivate key-pair)
+        ^java.util.Date from          (new java.util.Date)
+        ^java.util.Date to            (new java.util.Date
+                                           (-> from
+                                               .getTime
+                                               (+ (* days 86400000))))
+        ^CertificateValidity interval (new CertificateValidity from to)
+        ^BigInteger sn                (new BigInteger 64 (new SecureRandom))
+        ^X500Name owner               (new X500Name dn)
+        ^AlgorithmId algo             (new AlgorithmId
+                                           AlgorithmId/md5WithRSAEncryption_oid)
         ^X509CertInfo info
         (doto (new X509CertInfo)
           (.set X509CertInfo/VALIDITY interval)
@@ -61,31 +56,32 @@
                 (new CertificateSerialNumber sn))
           (.set X509CertInfo/SUBJECT owner)
           (.set X509CertInfo/ISSUER owner)
-          (.set X509CertInfo/KEY (new CertificateX509Key
-                                      (.getPublic key-pair)))
+          (.set X509CertInfo/KEY
+                (new CertificateX509Key
+                     (.getPublic key-pair)))
           (.set X509CertInfo/VERSION
                 (new CertificateVersion
                      CertificateVersion/V3))
-          (.set X509CertInfo/ALGORITHM_ID (new CertificateAlgorithmId
-                                               algo)))
+          (.set X509CertInfo/ALGORITHM_ID
+                (new CertificateAlgorithmId
+                     algo)))
 
-        ^X509CertImpl cert (doto (new X509CertImpl info)
-                             ;; Sign the cert to identify the algorithm
-                             (.sign private-key alg))
+        ^X509CertImpl cert      (doto (new X509CertImpl info)
+                                  ;; Sign the cert to identify the algorithm
+                                  (.sign private-key alg))
         ;; Update the algorithm, and re-sign.
         ^AlgorithmId algo-final (.get cert X509CertImpl/SIG_ALG)
         ;; mutation of the info carried from example :()
-        _ (.set info
-                (format
-                 "%s.%s"
-                 CertificateAlgorithmId/NAME
-                 CertificateAlgorithmId/ALGORITHM)
-                algo-final)
+        _                       (.set info
+                                      (format
+                                       "%s.%s"
+                                       CertificateAlgorithmId/NAME
+                                       CertificateAlgorithmId/ALGORITHM)
+                                      algo-final)
 
         ^X509CertImpl cert-final (doto (new X509CertImpl info)
-                                   (.sign private-key alg))
-        ]
-    {:cert cert-final
+                                   (.sign private-key alg))]
+    {:cert     cert-final
      :key-pair key-pair}))
 
 (defn selfie-keystore
