8dd18b6d7ca8b3daf3b63956f2f104114948587e
kelvinqian00 <kelvinqian2@gmail.com>
2021-08-05 15:34:48 -0400

    Normalize config.edn and denorm in util

--- src/main/lrsql/util.clj
+++ src/main/lrsql/util.clj
@@ -56,7 +56,12 @@
   "Read `config/config.edn` with the given value of `profile`. Valid
    profiles are `:test-[db-type]` and `:prod-[db-type]`."
   [profile]
-  (aero/read-config (io/resource "config.edn") {:profile profile}))
+  (let [{:keys [database connection lrs webserver]}
+        (aero/read-config (io/resource "config.edn")
+                          {:profile profile})]
+    {:connection (assoc connection :database database)
+     :lrs        lrs
+     :webserver  webserver}))
 
 (def read-config
   "Memoized version of `read-config*`."
