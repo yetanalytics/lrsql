d3d2ae242878c01ebe00e7c9dfcb8db65ba2dab0
kelvinqian00 <kelvinqian2@gmail.com>
2021-08-07 10:54:03 -0400

    Remove default template in lieu of config default

--- src/main/lrsql/util/authority.clj
+++ src/main/lrsql/util/authority.clj
@@ -5,8 +5,8 @@
             [clojure.java.io :as io]
             [clojure.spec.alpha :as s]
             [lrsql.spec.common :as c]
-            [selmer.parser :as parser]
-            [selmer.util :as su]
+            [selmer.parser :as selm-parser]
+            [selmer.util :as selm-u]
             [xapi-schema.spec :as xs])
   (:import [java.io File]))
 
@@ -20,7 +20,8 @@
                    ::account-id]))
 
 (defn throw-on-missing
-  "When a user enters a variable and it is not in our context map, throw!"
+  "When a user enters a variable and it is not in our context map, throw!
+   Used by selmer when context map validation fails."
   [tag context-map]
   (throw (ex-info
           (format "\"%s\" is not a valid variable for the authority template."
@@ -29,72 +30,60 @@
            :tag         tag
            :context-map context-map})))
 
-(def default-template
-  "The default precompiled template to render authority"
-  (-> "authority.json.template"
-      io/resource
-      parser/parse*))
-
-(def default-authority-function
-  (fn [context-map]
-    (binding [su/*missing-value-formatter* throw-on-missing
-              su/*filter-missing-values*   (constantly false)]
-      (-> default-template
-          (parser/render-template context-map)
-          json/parse-string))))
+(def sample-authority-fn-input
+  {:authority-url "https://lrs.example.com"
+   :cred-id       #uuid "00000000-0000-4000-0000-000000000001"
+   :account-id    #uuid "00000000-0000-4000-0000-000000000002"})
 
 (defn- valid-authority-fn?
   [authority-fn]
-  (s/valid?
-   ::xs/agent
-   (authority-fn
-    {:authority-url "https://lrs.example.com"
-     :cred-id       #uuid "41ec697d-802e-4f3e-aad5-e5fc9fb55f35"
-     :account-id    #uuid "3aa61cf9-a697-46f1-b60d-62a2c78ab33b"})))
+  (s/valid? ::xs/agent (authority-fn sample-authority-fn-input)))
 
-(s/fdef make-authority-fn*
+(s/fdef make-authority-fn
   :args (s/cat :template-path (s/nilable string?) :threshold (s/? pos-int?))
   :ret (s/fspec
         :args (s/cat :context-map ::context-map)
         :ret ::xs/agent))
 
+(defn make-authority-fn**
+  "Returns a function that will render the template to data, using `template`.
+   Not memoized."
+  [template]
+  (fn [context-map]
+    (binding [selm-u/*missing-value-formatter* throw-on-missing
+              selm-u/*filter-missing-values*   (constantly false)]
+      (-> template
+          (selm-parser/render-template context-map)
+          json/parse-string))))
+
 (defn make-authority-fn*
+  "Returns a function that will render the template to data, using the
+   template read at `template-path`. Not memoized."
   [template-path]
   (let [^File f (io/file template-path)]
     (if (and f (.exists f))
-      (try
-        (let [template (parser/parse* f)
-              authority-fn'
-              (fn [context-map]
-                (binding [su/*missing-value-formatter* throw-on-missing
-                          su/*filter-missing-values*   (constantly false)]
-                  (-> template
-                      (parser/render-template context-map)
-                      json/parse-string)))]
-          (if (valid-authority-fn? authority-fn')
-            authority-fn'
-            (throw
-             (ex-info "Authority template does not produce a valid xAPI Agent"
-                      {:type          ::invalid-json
-                       :template-path template-path}))))
-        (catch Throwable ex
+      (let [template     (selm-parser/parse* f)
+            authority-fn (make-authority-fn** template)]
+        (if (valid-authority-fn? authority-fn)
+          authority-fn
           (throw
-           (ex-info
-            (format "Invalid Authority Template specified at %s" template-path)
-            {:type          ::invalid-template
-             :template-path template-path}
-            ex))))
-      default-authority-function)))
+           (ex-info "Authority template does not produce a valid xAPI Agent"
+                    {:type          ::invalid-json
+                     :template-path template-path}))))
+      (throw
+       (ex-info (format "No authority template specified at %s" template-path)
+                {:type          ::no-authority-template
+                 :template-path template-path})))))
 
 (defn make-authority-fn
   "Returns a memoized function that will render the template to data.
-  Will use the template at the filesystem path template-path or the default if
-  no file is found.
-  Threshold controls the how many authorities will be cached before LRU clearing"
+   Will use the template at the filesystem path `template-path`; throws an
+   exception if the template is not found or if it is malformed.
+   The `threshold` optional arg controls the how many authorities will be
+   cached before least-recently used (LRU) clearing."
   [template-path & [threshold]]
-  (mem/lru
-   (make-authority-fn* template-path)
-   :lru/threshold (or threshold 512)))
+  (mem/lru (make-authority-fn* template-path)
+           :lru/threshold (or threshold 512)))
 
 (comment
 
@@ -112,13 +101,11 @@
   ;; only remember one authority ;; don't ever do this but just for example
   (let [a-fn (make-authority-fn "config/authority.json.template" 1)]
     (a-fn {:authority-url "https://lrs.example.com"
-           :cred-id       #uuid "41ec697d-802e-4f3e-aad5-e5fc9fb55f35"
+           :cred-id       #uuid "41ec697d-802e-4f3e-aad5-e5fc9fb55f36"
            :account-id    #uuid "3aa61cf9-a697-46f1-b60d-62a2c78ab33b"}))
 
   ;; Run `make-authority-fn` during startup and then pass the resulting function
   ;; to a suitable component. It will only ever render the agent once for a
   ;; given context map (input), until old entries are evicted when `threshold`
   ;; is reached
-
-
   )
