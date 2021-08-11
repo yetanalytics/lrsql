6b481f9faaf5e07462b383d453d2f0141dde2dc5
kelvinqian00 <kelvinqian2@gmail.com>
2021-08-11 11:32:25 -0400

    Remove config util file

--- src/main/lrsql/init/config.clj
+++ src/main/lrsql/init/config.clj
@@ -1,7 +1,31 @@
 (ns lrsql.init.config
-  (:require [clojure.java.io :as io]
+  (:require [cheshire.core :as json]
+            [clojure.java.io :as io]
             [aero.core :as aero]
-            [lrsql.util.config :as config-u]))
+            [camel-snake-kebab.core :as csk])
+  (:import [java.io File]))
+
+(defn- merge-user-config
+  "Given a static config map from aero and a path where a user config json file
+  might reside, deep merge in valid json."
+  [{:keys [user-config-json]
+    :as   static-config}]
+  (let [;; place handle on the config file at path
+        ^File config-file (io/file user-config-json)]
+    (merge-with
+     merge
+     static-config
+     ;; merge with user configuration if one is provided
+     (when (.exists config-file)
+       (try
+         (with-open [rdr (io/reader config-file)]
+           (json/parse-stream-strict rdr csk/->kebab-case-keyword))
+         (catch Exception ex
+           (throw
+            (ex-info "Invalid JSON in Config File"
+                     {:type ::invalid-user-config-json
+                      :path user-config-json}
+                     ex))))))))
 
 ;; The default aero `#include` resolver does not work with JARs, so we
 ;; need to resolve the root dirs manually.
@@ -27,7 +51,7 @@
                                 (aero/read-config
                                  {:profile  profile
                                   :resolver resolver})
-                                config-u/merge-user-config)]
+                                merge-user-config)]
     ;; form the final config the app will use
     {:connection (assoc connection :database database)
      :lrs        (assoc lrs :stmt-url-prefix (:url-prefix webserver))
