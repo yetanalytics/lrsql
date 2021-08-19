1026185e813405683a1ec76dec1769e1d82e6e37
kelvinqian00 <kelvinqian2@gmail.com>
2021-08-19 09:14:13 -0400

    Refactor scope tests + add some more

--- src/test/lrsql/util/auth_test.clj
+++ src/test/lrsql/util/auth_test.clj
@@ -24,64 +24,83 @@
 
 (deftest authorize-test
   (testing "authorize-action function"
-    (are [exp act] (= exp (:result act))
+    (are [expected input]
+         (= expected
+            (let [{:keys [request-method path-info scopes]} input]
+              (:result (au/authorize-action
+                        {:request {:request-method request-method
+                                   :path-info path-info}}
+                        {:scopes scopes}))))
       ;; all scope
-      true (au/authorize-action {:request {:request-method :get
-                                           :path-info "xapi/statements"}}
-                                {:scopes #{:scope/all}})
-      true (au/authorize-action {:request {:request-method :head
-                                           :path-info "xapi/statements"}}
-                                {:scopes #{:scope/all}})
-      true (au/authorize-action {:request {:request-method :put
-                                           :path-info "xapi/statements"}}
-                                {:scopes #{:scope/all}})
-      true (au/authorize-action {:request {:request-method :post
-                                           :path-info "xapi/statements"}}
-                                {:scopes #{:scope/all}})
+      true {:request-method :get
+            :path-info      "xapi/statements"
+            :scopes         #{:scope/all}}
+      true {:request-method :head
+            :path-info      "xapi/statements"
+            :scopes         #{:scope/all}}
+      true {:request-method :put
+            :path-info      "xapi/statements"
+            :scopes         #{:scope/all}}
+      true {:request-method :post
+            :path-info      "xapi/statements"
+            :scopes         #{:scope/all}}
       ;; all/read scope
-      true (au/authorize-action {:request {:request-method :get
-                                           :path-info "xapi/statements"}}
-                                {:scopes #{:scope/all.read}})
-      true (au/authorize-action {:request {:request-method :head
-                                           :path-info "xapi/statements"}}
-                                {:scopes #{:scope/all.read}})
-      false (au/authorize-action {:request {:request-method :put
-                                            :path-info "xapi/statements"}}
-                                 {:scopes #{:scope/all.read}})
-      false (au/authorize-action {:request {:request-method :post
-                                            :path-info "xapi/statements"}}
-                                 {:scopes #{:scope/all.read}})
+      true {:request-method :get
+            :path-info      "xapi/statements"
+            :scopes         #{:scope/all.read}}
+      true {:request-method :head
+            :path-info      "xapi/statements"
+            :scopes         #{:scope/all.read}}
+      false {:request-method :put
+             :path-info      "xapi/statements"
+             :scopes         #{:scope/all.read}}
+      false {:request-method :post
+             :path-info      "xapi/statements"
+             :scopes         #{:scope/all.read}}
       ;; statements/read scope
-      true (au/authorize-action {:request {:request-method :get
-                                           :path-info "xapi/statements"}}
-                                {:scopes #{:scope/statements.read}})
-      true (au/authorize-action {:request {:request-method :head
-                                           :path-info "xapi/statements"}}
-                                {:scopes #{:scope/statements.read}})
-      false (au/authorize-action {:request {:request-method :put
-                                            :path-info "xapi/statements"}}
-                                 {:scopes #{:scope/statements.read}})
-      false (au/authorize-action {:request {:request-method :post
-                                            :path-info "xapi/statements"}}
-                                 {:scopes #{:scope/statements.read}})
-      false (au/authorize-action {:request {:request-method :get
-                                            :path-info "xapi/activites/state"}}
-                                 {:scopes #{:scope/statements.read}})
+      true {:request-method :get
+            :path-info      "xapi/statements"
+            :scopes         #{:scope/statements.read}}
+      true {:request-method :head
+            :path-info      "xapi/statements"
+            :scopes         #{:scope/statements.read}}
+      false {:request-method :put
+             :path-info      "xapi/statements"
+             :scopes         #{:scope/statements.read}}
+      false {:request-method :post
+             :path-info      "xapi/statements"
+             :scopes         #{:scope/statements.read}}
+      false {:request-method :get
+             :path-info      "xapi/activities/state"
+             :scopes         #{:scope/statements.read}}
       ;; statements/write scope
-      true (au/authorize-action {:request {:request-method :put
-                                           :path-info "xapi/statements"}}
-                                {:scopes #{:scope/statements.write}})
-      true (au/authorize-action {:request {:request-method :post
-                                           :path-info "xapi/statements"}}
-                                {:scopes #{:scope/statements.write}})
-      false (au/authorize-action {:request {:request-method :get
-                                            :path-info "xapi/statements"}}
-                                 {:scopes #{:scope/statements.write}})
-      false (au/authorize-action {:request {:request-method :head
-                                            :path-info "xapi/statements"}}
-                                 {:scopes #{:scope/statements.write}})
-      false (au/authorize-action {:request {:request-method :put
-                                            :path-info "xapi/activites/state"}}
-                                 {:scopes #{:scope/statements.write}})))
+      true {:request-method :put
+            :path-info      "xapi/statements"
+            :scopes         #{:scope/statements.write}}
+      true {:request-method :post
+            :path-info      "xapi/statements"
+            :scopes         #{:scope/statements.write}}
+      false {:request-method :get
+             :path-info      "xapi/statements"
+             :scopes         #{:scope/statements.write}}
+      false {:request-method :head
+             :path-info      "xapi/statements"
+             :scopes         #{:scope/statements.write}}
+      false {:request-method :put
+             :path-info      "xapi/activities/state"
+             :scopes         #{:scope/statements.write}}
+      ;; Multiple scopes
+      true {:request-method :get
+            :path-info      "xapi/statements"
+            :scopes         #{:scope/statements.read :scope/statements.write}}
+      true {:request-method :post
+            :path-info      "xapi/statements"
+            :scopes         #{:scope/statements.read :scope/statements.write}}
+      true {:request-method :get
+            :path-info      "xapi/activities/state"
+            :scopes         #{:scope/all :scope/statements.read}}
+      true {:request-method :get
+            :path-info      "xapi/activities/state"
+            :scopes         #{:scope/all.read :scope/statements.read}}))
   (testing "authorize-action gentest"
     (is (nil? (check-validate `au/authorize-action)))))
