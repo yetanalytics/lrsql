9f8cb35fc4685f23a6911a1aab97e8fb5586b6d8
kelvinqian00 <kelvinqian2@gmail.com>
2021-08-30 11:18:50 -0400

    Fix failing CI and add tests

--- src/test/lrsql/util/database_test.clj
+++ src/test/lrsql/util/database_test.clj
@@ -1,8 +1,19 @@
 (ns lrsql.util.database-test
-  (:require [clojure.test :refer [deftest testing are]]
+  (:require [clojure.test :refer [deftest testing is are]]
+            [clojure.spec.alpha :as s]
+            [lrsql.spec.config :as cs]
             [lrsql.system.util :refer [parse-db-props]]))
 
 (deftest parse-db-props-test
+  (testing "validation regex"
+    (is (s/valid? ::cs/db-properties "foo=bar&baz=qux"))
+    (is (s/valid? ::cs/db-properties "currentSchema=lrsql,public"))
+    (is (s/valid? ::cs/db-properties "currentSchema=lrsql%2Cpublic"))
+    (is (s/valid? ::cs/db-properties "currentSchema=foo%26bar"))
+    (is (not (s/valid? ::cs/db-properties "currentSchema=foo&bar")))
+    (is (not (s/valid? ::cs/db-properties "currentSchema=foo&bar&baz=qux")))
+    (is (s/valid? ::cs/db-properties
+                  "options=-c%20search_path=lrsql,public,pgcatalog%20-c%20statement_timeout=90000")))
   (testing "parsing DB property strings"
     (are [m s] (= m (parse-db-props s))
       {:foo "bar"} "foo=bar"
