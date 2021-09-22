f343a2409f985aed5007999988f1be052903f93e
kelvinqian00 <kelvinqian2@gmail.com>
2021-09-22 15:18:26 -0400

    Update test cases

--- src/test/lrsql/util/statement_test.clj
+++ src/test/lrsql/util/statement_test.clj
@@ -2,6 +2,9 @@
   (:require [clojure.test :refer [deftest testing is]]
             [lrsql.util.statement :as su]))
 
+(def sample-id
+  "030e001f-b32a-4361-b701-039a3d9fceb1")
+
 (def sample-group
   {"mbox"       "mailto:sample.group@example.com"
    "name"       "Sample Group"
@@ -17,10 +20,10 @@
   {"mbox"       "mailto:sample.group@example.com"
    "name"       "Sample Group"
    "objectType" "Group"
-   "member"     #{{"mbox" "mailto:agent2@example.com"
-                   "name" "Agent 2"}
-                  {"mbox" "mailto:agent1@example.com"
-                   "name" "Agent 1"}}})
+   "member"     [{"mbox" "mailto:agent2@example.com"
+                  "name" "Agent 2"}
+                 {"mbox" "mailto:agent1@example.com"
+                  "name" "Agent 1"}]})
 
 (def sample-verb
   {"id"      "http://adlnet.gov/expapi/verbs/answered"
@@ -39,64 +42,122 @@
   {"id"         "http://www.example.com/tincan/activities/multipart"
    "objectType" "Activity"})
 
-(deftest dissoc-statement-properties-test
-  (testing "dissoc non-immutable statement properties"
-    (is (= {"actor"   sample-group-dissoc
+(deftest statements-equal-test
+  (testing "statement equality"
+    (is (su/statement-equal?
+         {"id"      sample-id
+          "actor"   sample-group-dissoc
+          "verb"    sample-verb-dissoc
+          "object"  sample-activity-dissoc
+          "context" {"instructor" sample-group-dissoc
+                     "team"       sample-group-dissoc
+                     "contextActivities"
+                     {"category" [sample-activity-dissoc]
+                      "parent"   [sample-activity-dissoc]
+                      "grouping" [sample-activity-dissoc]
+                      "other"    [sample-activity-dissoc]}}}
+         {"id"      sample-id
+          "actor"   sample-group
+          "verb"    sample-verb
+          "object"  sample-activity
+          "context" {"instructor" sample-group
+                     "team"       sample-group
+                     "contextActivities"
+                     {"category" [sample-activity]
+                      "parent"   [sample-activity]
+                      "grouping" [sample-activity]
+                      "other"    [sample-activity]}}}))
+    (is (su/statement-equal?
+         {"id"      sample-id
+          "actor"   sample-group
+          "verb"    sample-verb
+          "object"  sample-activity
+          "context" {"instructor" sample-group
+                     "team"       sample-group
+                     "contextActivities"
+                     {"category" [sample-activity]
+                      "parent"   [sample-activity]
+                      "grouping" [sample-activity]
+                      "other"    [sample-activity]}}}
+         {"id"      sample-id
+          "actor"   sample-group
+          "verb"    sample-verb
+          "object"  sample-activity
+          "context" {"instructor" sample-group
+                     "team"       sample-group
+                     "contextActivities"
+                     {"category" [sample-activity
+                                  sample-activity]
+                      "parent"   [sample-activity
+                                  sample-activity]
+                      "grouping" [sample-activity
+                                  sample-activity]
+                      "other"    [sample-activity
+                                  sample-activity]}}}))
+    (testing "(with SubStatement)"
+      (is (su/statement-equal?
+           {"id"      sample-id
+            "actor"   sample-group-dissoc
             "verb"    sample-verb-dissoc
-            "object"  sample-activity-dissoc
-            "context" {"instructor" sample-group-dissoc
-                       "team"       sample-group-dissoc
-                       "contextActivities"
-                       {"category" #{sample-activity-dissoc}
-                        "parent"   #{sample-activity-dissoc}
-                        "grouping" #{sample-activity-dissoc}
-                        "other"    #{sample-activity-dissoc}}}}
-           (su/dissoc-statement-properties
-            {"id"      "030e001f-b32a-4361-b701-039a3d9fceb1"
-             "actor"   sample-group
-             "verb"    sample-verb
-             "object"  sample-activity
-             "context" {"instructor" sample-group
-                        "team"       sample-group
-                        "contextActivities"
-                        {"category" [sample-activity
-                                     sample-activity]
-                         "parent"   [sample-activity
-                                     sample-activity]
-                         "grouping" [sample-activity
-                                     sample-activity]
-                         "other"    [sample-activity
-                                     sample-activity]}}})))
-    (is (= {"actor" sample-group-dissoc
-            "verb"  sample-verb-dissoc
-            "object" {"objectType" "SubStatement"
-                      "actor"      sample-group-dissoc
-                      "verb"       sample-verb-dissoc
-                      "object"     sample-activity-dissoc
-                      "context"
-                      {"instructor" sample-group-dissoc
-                       "team"       sample-group-dissoc
-                       "contextActivities"
-                       {"category" #{sample-activity-dissoc}
-                        "parent"   #{sample-activity-dissoc}
-                        "grouping" #{sample-activity-dissoc}
-                        "other"    #{sample-activity-dissoc}}}}}
-           (su/dissoc-statement-properties
-            {"id"      "030e001f-b32a-4361-b701-039a3d9fceb1"
-             "actor"   sample-group
-             "verb"    sample-verb
-             "object"  {"objectType" "SubStatement"
-                        "actor"   sample-group
-                        "verb"    sample-verb
-                        "object"  sample-activity
-                        "context" {"instructor" sample-group
-                                   "team"       sample-group
-                                   "contextActivities"
-                                   {"category" [sample-activity
-                                                sample-activity]
-                                    "parent"   [sample-activity
-                                                sample-activity]
-                                    "grouping" [sample-activity
-                                                sample-activity]
-                                    "other"    [sample-activity
-                                                sample-activity]}}}})))))
+            "object"  {"objectType" "SubStatement"
+                       "actor"   sample-group-dissoc
+                       "verb"    sample-verb-dissoc
+                       "object"  sample-activity-dissoc
+                       "context" {"instructor" sample-group-dissoc
+                                  "team"       sample-group-dissoc
+                                  "contextActivities"
+                                  {"category" [sample-activity-dissoc]
+                                   "parent"   [sample-activity-dissoc]
+                                   "grouping" [sample-activity-dissoc]
+                                   "other"    [sample-activity-dissoc]}}}}
+           {"id"      sample-id
+            "actor"   sample-group
+            "verb"    sample-verb
+            "object"  {"objectType" "SubStatement"
+                       "actor"   sample-group
+                       "verb"    sample-verb
+                       "object"  sample-activity
+                       "context" {"instructor" sample-group
+                                  "team"       sample-group
+                                  "contextActivities"
+                                  {"category" [sample-activity
+                                               sample-activity]
+                                   "parent"   [sample-activity
+                                               sample-activity]
+                                   "grouping" [sample-activity
+                                               sample-activity]
+                                   "other"    [sample-activity
+                                               sample-activity]}}}}))
+      (is (su/statement-equal?
+           {"id"      sample-id
+            "actor"   sample-group
+            "verb"    sample-verb
+            "object"  {"objectType" "SubStatement"
+                       "actor"   sample-group
+                       "verb"    sample-verb
+                       "object"  sample-activity
+                       "context" {"instructor" sample-group
+                                  "team"       sample-group
+                                  "contextActivities"
+                                  {"category" [sample-activity]
+                                   "parent"   [sample-activity]
+                                   "grouping" [sample-activity]
+                                   "other"    [sample-activity]}}}}
+           {"id"      sample-id
+            "actor"   sample-group
+            "verb"    sample-verb
+            "object"  {"objectType" "SubStatement"
+                       "actor"   sample-group
+                       "verb"    sample-verb
+                       "object"  sample-activity
+                       "context" {"instructor" sample-group
+                                  "team"       sample-group
+                                  "contextActivities"
+                                  {"category" [sample-activity
+                                               sample-activity]
+                                   "parent"   [sample-activity
+                                               sample-activity]
+                                   "grouping" [sample-activity
+                                               sample-activity]
+                                   "other"    [sample-activity
+                                               sample-activity]}}}})))))
