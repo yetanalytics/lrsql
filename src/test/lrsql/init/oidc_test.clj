5a5783bc6faf8fa7b248f3810cc2a013bbb070cd
Milton Reder <milt@yetanalytics.com>
2022-03-23 13:08:08 -0400

    SQL-138 Admin UI OIDC (#210)
    * SQL-138 Admin env template and config
    
    * SQL-138 provide OIDC config.
    
    * SQL-138 add OIDC redirects that preserve qstring
    
    * SQL-138 separate oidc redirects
    
    * SQL-138 fix render test
    
    * SQL-138 remove concept of server-side redirect
    
    * SQL-138 use prelease of OIDC-enabled admin ui
    
    * SQL-138 latest ui with correct push-state
    
    * SQL-138 move :oidc-client-id to webserver config
    
    * SQL-138 env var doc for OIDC client template
    
    * SQL-138 move oidc client template to webserver
    
    * SQL-138 utilize jwt-exp-leeway
    
    * SQL-142 document that jwt-exp-leeway applies to all unsigned tokens.
    
    * SQL-138 refactor OIDC interceptors init fn

--- src/test/lrsql/init/oidc_test.clj
+++ src/test/lrsql/init/oidc_test.clj
@@ -1,8 +1,12 @@
 (ns lrsql.init.oidc-test
-  (:require [clojure.test    :refer [deftest is testing are]]
-            [lrsql.init.oidc :refer [get-configuration
-                                     resolve-authority-claims
-                                     make-authority-fn]]))
+  (:require [clojure.test       :refer [deftest is testing are]]
+            [lrsql.init.oidc    :refer [get-configuration
+                                        resolve-authority-claims
+                                        make-authority-fn
+                                        render-client-config]]
+            [lrsql.test-support :refer [instrument-lrsql]]))
+
+(instrument-lrsql)
 
 (deftest get-configuration-test
   (testing "Slurps configuration"
@@ -56,3 +60,22 @@
                  :iss   "foo"
                  :aud   "bar"
                  :sub   "baz"}))))))
+
+(deftest render-client-config-test
+  (testing "Renders OIDC Client config."
+    (is
+     (= {"authority"                "https://idp.example.com/realm",
+         "automaticSilentRenew"     true,
+         "extraQueryParams"         {"audience" "https://lrs.example.com"},
+         "monitorSession"           false,
+         "filterProtocolClaims"     false,
+         "response_type"            "code",
+         "scope"                    "openid profile lrs:admin",
+         "client_id"                "1234"}
+        (render-client-config
+         {:webserver {:oidc-issuer    "https://idp.example.com/realm"
+                      :oidc-audience  "https://lrs.example.com"
+                      :oidc-client-id "1234"
+                      :oidc-client-template
+                      "resources/lrsql/config/oidc_client.json.template"}
+          :lrs       {:oidc-scope-prefix "lrs:"}})))))
