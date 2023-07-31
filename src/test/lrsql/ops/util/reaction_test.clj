7c6bf6d27cda37264fe65890cecba79836a7117b
Milton Reder <milt@yetanalytics.com>
2023-07-31 16:12:24 -0400

    SQL-198 refactor some more test consts

--- src/test/lrsql/ops/util/reaction_test.clj
+++ src/test/lrsql/ops/util/reaction_test.clj
@@ -29,53 +29,6 @@
 (use-fixtures :once support/instrumentation-fixture)
 (use-fixtures :each support/fresh-db-fixture)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Test content
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(def stmt-a
-  {"id"     "6fbd600f-d17c-4c74-801a-2ec2e53231f7"
-   "actor"  {"mbox" "mailto:bob@example.com"}
-   "verb"   {"id" "https://example.com/verbs/completed"}
-   "object" {"id"         "https://example.com/activities/a"
-             "objectType" "Activity"}
-   "result" {"success" true}})
-
-(def stmt-b
-  {"id"     "c51d1628-ae4a-449f-8d8d-13d57207f468"
-   "actor"  {"mbox" "mailto:bob@example.com"}
-   "verb"   {"id" "https://example.com/verbs/completed"}
-   "object" {"id"         "https://example.com/activities/b"
-             "objectType" "Activity"}
-   "result" {"success" true}})
-
-;; Same actor, wrong activity
-(def stmt-c
-  {"id"     "5716d2c3-1ed3-4646-9475-a6b3f3dc5d66"
-   "actor"  {"mbox" "mailto:bob@example.com"}
-   "verb"   {"id" "https://example.com/verbs/completed"}
-   "object" {"id"         "https://example.com/activities/c"
-             "objectType" "Activity"}
-   "result" {"success" true}})
-
-;; Different actor, same action as a
-(def stmt-d
-  {"id"      "da38014b-371d-4549-8f9f-e05193b89998"
-   "actor"   {"mbox" "mailto:alice@example.com"}
-   "verb"    {"id" "https://example.com/verbs/completed"}
-   "object"  {"id"         "https://example.com/activities/a"
-              "objectType" "Activity"}
-   "result"  {"success" true}
-   "context" {"extensions"
-              {"https://example.com/array"  ["foo" "bar" "baz"]
-               "https://example.com/number" 200}}})
-
-(def auth-ident
-  {:agent  {"objectType" "Agent"
-            "account"    {"homePage" "http://example.org"
-                          "name"     "12341234-0000-4000-1234-123412341234"}}
-   :scopes #{:scope/all}})
-
 (deftest query-reaction-test
   (let [sys  (support/test-system)
         sys' (component/start sys)
@@ -83,30 +36,35 @@
         bk   (:backend lrs)
         ds   (-> sys' :lrs :connection :conn-pool)]
 
-    (doseq [s [stmt-d stmt-a stmt-b stmt-c]]
+    (doseq [s [tc/reaction-stmt-d
+               tc/reaction-stmt-a
+               tc/reaction-stmt-b
+               tc/reaction-stmt-c]]
       (Thread/sleep 100)
-      (lrsp/-store-statements lrs auth-ident [s] []))
+      (lrsp/-store-statements lrs tc/auth-ident [s] []))
 
     (try
       (testing "Returns relevant statements"
         (let [query-result (ur/query-reaction
                             bk ds
                             {:ruleset    tc/simple-reaction-ruleset
-                             :trigger-id (u/str->uuid (get stmt-b "id"))
+                             :trigger-id (u/str->uuid
+                                          (get tc/reaction-stmt-b "id"))
                              :statement-identity
                              {["actor" "mbox"] "mailto:bob@example.com"}})]
           ;; unambiguous, finds only a single row with a and b
           (is (= 1 (count query-result)))
           (let [[{:keys [a b]}] query-result]
-            (is (= stmt-a (remove-props a)))
-            (is (= stmt-b (remove-props b))))))
+            (is (= tc/reaction-stmt-a (remove-props a)))
+            (is (= tc/reaction-stmt-b (remove-props b))))))
       (testing "Works w/o identity"
         (let [query-result (ur/query-reaction
                             bk ds
                             {:ruleset
                              (merge tc/simple-reaction-ruleset
                                     {:identity-paths []})
-                             :trigger-id         (u/str->uuid (get stmt-b "id"))
+                             :trigger-id         (u/str->uuid
+                                                  (get tc/reaction-stmt-b "id"))
                              :statement-identity {}})]
           ;; ambiguous, finds a and b but ALSO d and b
           (is (= 2 (count query-result)))))
@@ -123,12 +81,13 @@
                                                 "https://example.com/array"]
                                          :op   "contains"
                                          :val  "bar"}]}}})
-                             :trigger-id (u/str->uuid (get stmt-d "id"))
+                             :trigger-id (u/str->uuid
+                                          (get tc/reaction-stmt-d "id"))
                              :statement-identity
                              {["actor" "mbox"] "mailto:alice@example.com"}})]
           (is (= 1 (count query-result)))
           (let [[{:keys [a]}] query-result]
-            (is (= stmt-d (remove-props a))))))
+            (is (= tc/reaction-stmt-d (remove-props a))))))
       (testing "Numeric types"
         ;; If it is just using lex, "200" > "1000"
         ;; Therefore we make sure it can compare numbers correctly
@@ -144,12 +103,13 @@
                                                 "https://example.com/number"]
                                          :op   "lt"
                                          :val  1000}]}}})
-                             :trigger-id (u/str->uuid (get stmt-d "id"))
+                             :trigger-id (u/str->uuid
+                                          (get tc/reaction-stmt-d "id"))
                              :statement-identity
                              {["actor" "mbox"] "mailto:alice@example.com"}})]
           (is (= 1 (count query-result)))
           (let [[{:keys [a]}] query-result]
-            (is (= stmt-d (remove-props a))))))
+            (is (= tc/reaction-stmt-d (remove-props a))))))
       (finally (component/stop sys')))))
 
 (deftest query-active-reactions-test
@@ -163,7 +123,6 @@
     (adp/-create-reaction lrs tc/simple-reaction-ruleset true)
     ;; Create an inactive reaciton
     (adp/-create-reaction lrs tc/simple-reaction-ruleset false)
-
     (try
       (testing "Finds only active reactions"
         (is (= [{:ruleset tc/simple-reaction-ruleset}]
@@ -187,23 +146,26 @@
         [a-id
          b-id
          c-id
-         d-id]     (for [stmt [stmt-a stmt-b stmt-c stmt-d]]
+         d-id]     (for [stmt [tc/reaction-stmt-a
+                               tc/reaction-stmt-b
+                               tc/reaction-stmt-c
+                               tc/reaction-stmt-d]]
                      (u/str->uuid (get stmt "id")))]
 
     ;; store a statements with chained reaciton data
     (lrsp/-store-statements lrs
-                            auth-ident
-                            [stmt-a
+                            tc/auth-ident
+                            [tc/reaction-stmt-a
                              (ru/add-reaction-metadata
-                              stmt-b
+                              tc/reaction-stmt-b
                               reaction-0-id
                               a-id)
                              (ru/add-reaction-metadata
-                              stmt-c
+                              tc/reaction-stmt-c
                               reaction-1-id
                               b-id)
                              (ru/add-reaction-metadata
-                              stmt-d
+                              tc/reaction-stmt-d
                               reaction-2-id
                               c-id)]
                             [])
--- src/test/lrsql/test_constants.clj
+++ src/test/lrsql/test_constants.clj
@@ -1,5 +1,19 @@
 (ns lrsql.test-constants)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; General
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def auth-ident
+  {:agent  {"objectType" "Agent"
+            "account"    {"homePage" "http://example.org"
+                          "name"     "12341234-0000-4000-1234-123412341234"}}
+   :scopes #{:scope/all}})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Reactions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (def simple-reaction-ruleset
   {:identity-paths [["actor" "mbox"]]
    :template
@@ -33,3 +47,40 @@
       {:path ["timestamp"]
        :op   "gt"
        :ref  {:condition "a", :path ["timestamp"]}}]}}})
+
+(def reaction-stmt-a
+  {"id"     "6fbd600f-d17c-4c74-801a-2ec2e53231f7"
+   "actor"  {"mbox" "mailto:bob@example.com"}
+   "verb"   {"id" "https://example.com/verbs/completed"}
+   "object" {"id"         "https://example.com/activities/a"
+             "objectType" "Activity"}
+   "result" {"success" true}})
+
+(def reaction-stmt-b
+  {"id"     "c51d1628-ae4a-449f-8d8d-13d57207f468"
+   "actor"  {"mbox" "mailto:bob@example.com"}
+   "verb"   {"id" "https://example.com/verbs/completed"}
+   "object" {"id"         "https://example.com/activities/b"
+             "objectType" "Activity"}
+   "result" {"success" true}})
+
+;; Same actor, wrong activity
+(def reaction-stmt-c
+  {"id"     "5716d2c3-1ed3-4646-9475-a6b3f3dc5d66"
+   "actor"  {"mbox" "mailto:bob@example.com"}
+   "verb"   {"id" "https://example.com/verbs/completed"}
+   "object" {"id"         "https://example.com/activities/c"
+             "objectType" "Activity"}
+   "result" {"success" true}})
+
+;; Different actor, same action as a
+(def reaction-stmt-d
+  {"id"      "da38014b-371d-4549-8f9f-e05193b89998"
+   "actor"   {"mbox" "mailto:alice@example.com"}
+   "verb"    {"id" "https://example.com/verbs/completed"}
+   "object"  {"id"         "https://example.com/activities/a"
+              "objectType" "Activity"}
+   "result"  {"success" true}
+   "context" {"extensions"
+              {"https://example.com/array"  ["foo" "bar" "baz"]
+               "https://example.com/number" 200}}})
