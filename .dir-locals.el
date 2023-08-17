d4ee12a61f856ea002a108c337e274e45d2e0d0c
Milton Reder <milt@yetanalytics.com>
2023-08-17 17:01:26 -0400

    SQL-203 Bench Insert Timings (#319)
    * SQL-203 add bench alias for emacs
    
    * SQL-203 return stats for insert
    
    * SQL-203 report insert finish before table as in query

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil .
-      ((cider-clojure-cli-global-options . "-A:test:build-dev")
+      ((cider-clojure-cli-global-options . "-A:test:bench:build-dev")
        )))
