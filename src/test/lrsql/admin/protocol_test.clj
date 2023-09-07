56173239774f3a9b1add926e2bf554ff94d1d247
Daniel Bell <daniel@yetanalytics.com>
2023-09-07 07:16:11 -0700

    adds tests

--- src/test/lrsql/admin/protocol_test.clj
+++ src/test/lrsql/admin/protocol_test.clj
@@ -6,7 +6,8 @@
             [xapi-schema.spec.regex :refer [Base64RegEx]]
             [lrsql.admin.protocol :as adp]
             [lrsql.test-support   :as support]
-            [lrsql.util           :as u]))
+            [lrsql.util           :as u]
+            [lrsql.util.actor     :as ua]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Init
@@ -51,6 +52,33 @@
                          "zh-CN" "回答了"}}
    "object"  {"id" "http://www.example.com/tincan/activities/multipart"}
    "context" {"platform" "another_example"}})
+
+;; A third statement with a different actor, referring to stmt-0
+(def stmt-2
+  {"id"     "00000000-0000-4000-8000-000000000002"
+   "actor"  {"account"    {"name"     "Sample Agent 2"
+                           "homePage" "https://example.org"}
+             "name"       "Sample Agent 2"
+             "objectType" "Agent"}
+   "verb"   {"id"      "http://adlnet.gov/expapi/verbs/asked"
+             "display" {"en-US" "asked"}}
+   "object" {"objectType" "StatementRef"
+             "id"         "00000000-0000-4000-8000-000000000000"}})
+
+;; A fourth statement (with a third actor) containing a substatement (by a fourth actor
+(def stmt-3
+  {"id"     "00000000-0000-4000-8000-000000000003"
+   "actor"   {"mbox"       "mailto:sample.bar@example.com"
+              "objectType" "Agent"}
+   "verb"   {"id"      "http://adlnet.gov/expapi/verbs/asked"
+             "display" {"en-US" "asked"}}
+   "object"  {"objectType" "SubStatement"
+                       "actor"      {"mbox"       "mailto:sample.baz@example.com"
+                                     "objectType" "Agent"}
+                       "verb"       {"id"      "http://adlnet.gov/expapi/verbs/answered"
+                                     "display" {"en-US" "answered"}}
+                       "object"    {"id" "http://www.example.com/tincan/activities/multipart"}
+}})
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Tests
@@ -155,6 +183,49 @@
         (is (-> (adp/-ensure-account-oidc lrs username bad-issuer)
                 :result
                 (= :lrsql.admin/oidc-issuer-mismatch-error)))))
+    (testing "actor-delete"
+      (let [stmts  [stmt-0 stmt-1 stmt-2 stmt-3]
+            ifis (->> (conj stmts (stmt-3 "object"))
+                      (map #(ua/actor->ifi (% "actor"))))
+            get-stmt-#s (fn []
+                          (reduce (fn [m actor-ifi]
+                                    (assoc m actor-ifi
+                                           (count (:statements (:statement-result (lrsp/-get-statements lrs auth-ident {:actor-ifi actor-ifi} []))))))
+                                  {} ifis))]
+        (lrsp/-store-statements lrs auth-ident stmts [])
+
+        (assert (every? #(> % 0) (vals (get-stmt-#s))))
+        (doseq [ifi ifis]
+          (adp/-delete-actor lrs {:actor-ifi ifi}))
+        (is (every? #(= % 0) (vals (get-stmt-#s))))))
+    (let [stmt->ifi #(ua/actor->ifi (% "actor"))
+          count-of-actor (fn [actor-ifi] (-> (lrsp/-get-statements lrs auth-ident {:actor-ifi actor-ifi} []) :statement-result :statements count))
+          child-ifi (stmt->ifi (stmt-3 "object"))
+          parent-ifi (stmt->ifi stmt-3)]
+      (testing "substatement up"
+        (lrsp/-store-statements lrs auth-ident [stmt-3] [])
+        (assert (= 1 (count-of-actor parent-ifi)))
+        (adp/-delete-actor lrs {:actor-ifi child-ifi})
+        (is (= 0 (count-of-actor parent-ifi)));
+        (adp/-delete-actor lrs {:actor-ifi parent-ifi}))
+      (testing "substatement down"
+        (lrsp/-store-statements lrs auth-ident [stmt-3] [])
+        (assert (= 1 (count-of-actor child-ifi)))
+        (adp/-delete-actor lrs {:actor-ifi parent-ifi})
+        (is (= 0 (count-of-actor child-ifi)))
+        (adp/-delete-actor lrs {:actor-ifi child-ifi}))
+      (testing "statement ref connections deleted"
+        (let [[ifi-0 ifi-2] (mapv stmt->ifi [stmt-0 stmt-2])]
+          (lrsp/-store-statements lrs auth-ident [stmt-0] [])
+          (lrsp/-store-statements lrs auth-ident [stmt-2] [])      
+          (assert (= 2 (count-of-actor ifi-0)))
+          (assert (= 2 (count-of-actor ifi-2)))
+          (adp/-delete-actor lrs {:actor-ifi ifi-0})
+          (is (= 1 (count-of-actor ifi-0)))
+          (is (= 1 (count-of-actor ifi-2)))
+          (adp/-delete-actor lrs {:actor-ifi ifi-2})
+          (is (= 0 (count-of-actor ifi-0)))
+          (is (= 0 (count-of-actor ifi-2))))))
     (component/stop sys')))
 
 ;; TODO: Add tests for creds with no explicit scopes, once
