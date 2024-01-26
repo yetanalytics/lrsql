84761394d1261d4244641b47fb79b135065a0a50
kelvinqian00 <kelvinqian2@gmail.com>
2024-01-26 14:13:41 -0500

    Forgot to add commas

--- src/db/postgres/lrsql/postgres/sql/ddl.sql
+++ src/db/postgres/lrsql/postgres/sql/ddl.sql
@@ -304,9 +304,9 @@ CREATE TYPE scope_enum AS ENUM (
   'state/read',
   'define',
   -- new; add document/profile scopes (also document/state out of being pedantic)
-  'activity_profile'
-  'activity_profile/read'
-  'agent_profile'
+  'activity_profile',
+  'activity_profile/read',
+  'agent_profile',
   'agent_profile/read');
 ALTER TABLE IF EXISTS credential_to_scope ALTER COLUMN scope TYPE scope_enum USING (scope::scope_enum);
 
