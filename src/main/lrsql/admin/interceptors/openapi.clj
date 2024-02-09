6099fa289db2e9013ee079998a8d8d232a573c39
Daniel Bell <daniel@yetanalytics.com>
2024-02-09 13:22:59 -0600

    making marie happy

--- src/main/lrsql/admin/interceptors/openapi.clj
+++ src/main/lrsql/admin/interceptors/openapi.clj
@@ -1,18 +1,11 @@
 (ns lrsql.admin.interceptors.openapi
-  (:require [clojure.spec.alpha :as s]
-            [io.pedestal.interceptor :refer [interceptor]]
-            [io.pedestal.interceptor.chain :as chain]
-            [lrsql.admin.protocol :as adp]
-            [lrsql.spec.admin :as ads]
-            [lrsql.util.admin :as admin-u]
-            [lrsql.admin.interceptors.jwt :as jwt]
-            [lrsql.util :as u]))
+  (:require [io.pedestal.interceptor :refer [interceptor]]))
 
 (def openapi
   "Returns an openapi spec in json form for lrsql"
   (interceptor
    {:name ::openapi
     :enter
-    (fn openapi [ctx]
+    (fn openapi [_ctx]
       {:status 200
        :body (slurp "/resources/doc/openapi.json")})}))
