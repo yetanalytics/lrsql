b531e05a61d7c51a2f0f13fdb0c5a189b3cd098e
kelvinqian00 <kelvinqian2@gmail.com>
2024-03-27 15:45:45 -0400

    Add clj-kondo ignore to extend-protocol

--- src/main/lrsql/backend/data.clj
+++ src/main/lrsql/backend/data.clj
@@ -38,8 +38,13 @@
   [json-labels json-kw-labels]
   ;; Note: due to a long-standing bug, the byte array extension needs to come
   ;; first: https://clojure.atlassian.net/browse/CLJ-1381#icft=CLJ-1381
+  ;;
+  ;; Note: clj-kondo does not recognize the use of a non-simple-symbol in
+  ;; `extend-protocol` so we add the ignore (make sure to comment it out if
+  ;; you're editing this code).
+  #_{:clj-kondo/ignore [:syntax]}
   (extend-protocol ReadableColumn
-    (Class/forName "[B")
+    (Class/forName "[B") ; Evaulates to `[B`
     (read-column-by-label [^"[B" b ^String label]
       (parse-json-payload json-labels json-kw-labels label b))
     (read-column-by-index [^"[B" b ^ResultSetMetaData rsmeta ^long i]
