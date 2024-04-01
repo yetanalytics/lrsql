76704c372d7aaf3eb87c0401f9feceefd3c94de9
Henk Reder <henkreder@Henks-MacBook-Pro.local>
2024-04-01 18:55:57 -0400

    added trigger-update for AMI

---
+++ dev-resources/scripts/ami/trigger-update.sh
@@ -0,0 +1,40 @@
+# !/bin/bash
+
+# TODO: ADD this to GH Actions and get version dynamically when action is called
+VERSION="0.7.11"
+COMPONENT_ARN=$(aws imagebuilder list-components | jq -r '.componentVersionList[] | select(.name == "InstallLRSQL").arn')
+PIPELINE_ARN=$(aws imagebuilder list-image-pipelines | jq -r '.imagePipelineList[] | select(.name == "lrsql-ami-pipeline").arn')
+INFRA_ARN=$(aws imagebuilder list-image-pipelines | jq -r '.imagePipelineList[] | select(.name == "lrsql-ami-pipeline").infrastructureConfigurationArn')
+
+COMPONENTS=$(jq -n \
+    --arg arn "$COMPONENT_ARN" \
+    --arg version "v$VERSION" \
+'[
+  {
+    "componentArn": $arn,
+    "parameters": 
+    [
+      {
+        "name": "Version",
+        "value": [$version]
+      }
+    ]
+  }
+]'
+)
+
+## create new recipe
+IMAGE_RECIPE_ARN=$(aws imagebuilder create-image-recipe \
+                     --name "lrsql-ami" \
+                     --semantic-version $VERSION \
+                     --parent-image "arn:aws:imagebuilder:us-east-1:aws:image/amazon-linux-2023-x86/x.x.x" \
+                     --components "$COMPONENTS" | jq -r '.imageRecipeArn')
+
+
+# update the image pipeline with new recipe
+aws imagebuilder update-image-pipeline \
+  --image-pipeline-arn $PIPELINE_ARN \
+  --image-recipe-arn $IMAGE_RECIPE_ARN \
+  --infrastructure-configuration-arn $INFRA_ARN
+
+# TODO: add step that triggers the build
