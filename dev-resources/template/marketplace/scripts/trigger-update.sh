cab66a0825bc71dbc7260a8e4932cc0685a632e6
Henk Reder <henk@yetanalytics.com>
2024-05-13 08:59:20 -0400

    first pass at integrating ami build into GH actions

--- dev-resources/template/marketplace/scripts/trigger-update.sh
+++ dev-resources/template/marketplace/scripts/trigger-update.sh
@@ -1,42 +1,59 @@
 #!/bin/bash
 
-# TODO: ADD this to GH Actions and get version dynamically when new LRSQL release is cut
-VERSION="0.7.13"
-COMPONENT_ARN=$(aws imagebuilder list-components | jq -r '.componentVersionList[] | select(.name == "InstallLRSQL").arn')
-PIPELINE_ARN=$(aws imagebuilder list-image-pipelines | jq -r '.imagePipelineList[] | select(.name == "lrsql-ami-pipeline").arn')
-INFRA_ARN=$(aws imagebuilder list-image-pipelines | jq -r '.imagePipelineList[] | select(.name == "lrsql-ami-pipeline").infrastructureConfigurationArn')
+trigger_ami_build() {
+  REGION=$1
+  VERSION=$2
+  
+  COMPONENT_ARN=$(aws imagebuilder list-components | jq -r '.componentVersionList[] | select(.name == "InstallLRSQL").arn')
+  PIPELINE_ARN=$(aws imagebuilder list-image-pipelines | jq -r '.imagePipelineList[] | select(.name == "lrsql-ami-pipeline").arn')
+  INFRA_ARN=$(aws imagebuilder list-image-pipelines | jq -r '.imagePipelineList[] | select(.name == "lrsql-ami-pipeline").infrastructureConfigurationArn')
+  
+  COMPONENTS=$(jq -n \
+      --arg arn "$COMPONENT_ARN" \
+      --arg version "v$VERSION" \
+  '[
+    {
+      "componentArn": $arn,
+      "parameters": 
+      [
+        {
+          "name": "Version",
+          "value": [$version]
+        }
+      ]
+    }
+  ]'
+  )
+  
+  # create new recipe
+  IMAGE_RECIPE_ARN=$(aws imagebuilder create-image-recipe \
+                       --name "lrsql-ami-$REGION" \
+                       --semantic-version $VERSION \
+                       --parent-image "arn:aws:imagebuilder:$REGION:aws:image/amazon-linux-2023-x86/x.x.x" \
+                       --components "$COMPONENTS" | jq -r '.imageRecipeArn')
+  
+  
+  # update the image pipeline with new recipe
+  aws imagebuilder update-image-pipeline \
+    --image-pipeline-arn $PIPELINE_ARN \
+    --image-recipe-arn $IMAGE_RECIPE_ARN \
+    --infrastructure-configuration-arn $INFRA_ARN
+  
+  # triggers the build
+  aws imagebuilder start-image-pipeline-execution \
+    --image-pipeline-arn $PIPELINE_ARN
+}
 
-COMPONENTS=$(jq -n \
-    --arg arn "$COMPONENT_ARN" \
-    --arg version "v$VERSION" \
-'[
-  {
-    "componentArn": $arn,
-    "parameters": 
-    [
-      {
-        "name": "Version",
-        "value": [$version]
-      }
-    ]
-  }
-]'
-)
 
-# create new recipe
-IMAGE_RECIPE_ARN=$(aws imagebuilder create-image-recipe \
-                     --name "lrsql-ami" \
-                     --semantic-version $VERSION \
-                     --parent-image "arn:aws:imagebuilder:us-east-1:aws:image/amazon-linux-2023-x86/x.x.x" \
-                     --components "$COMPONENTS" | jq -r '.imageRecipeArn')
+# Check if script is being run directly and handle command line arguments
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    while getopts "r:v:" opt; do
+        case $opt in
+            r) REGION="$OPTARG";;
+            v) VERSION="$OPTARG";;
+            *) echo "Usage: $0 [-r region] [-v version]"; exit 1;;
+        esac
+    done
 
-
-# update the image pipeline with new recipe
-aws imagebuilder update-image-pipeline \
-  --image-pipeline-arn $PIPELINE_ARN \
-  --image-recipe-arn $IMAGE_RECIPE_ARN \
-  --infrastructure-configuration-arn $INFRA_ARN
-
-# triggers the build
-aws imagebuilder start-image-pipeline-execution \
-  --image-pipeline-arn $PIPELINE_ARN
+    trigger_ami_build $REGION $VERSION
+fi
