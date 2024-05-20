b2ed3dd0260054bedeef0ecc25c2edabc8b5a197
kelvinqian00 <kelvinqian2@gmail.com>
2024-05-20 16:57:55 -0400

    Add prepare-statement test cases

--- src/test/lrsql/util/statement_test.clj
+++ src/test/lrsql/util/statement_test.clj
@@ -1,6 +1,7 @@
 (ns lrsql.util.statement-test
   (:require [clojure.test :refer [deftest testing is]]
-            [lrsql.util.statement :as su]))
+            [lrsql.util.statement :as su]
+            [lrsql.util :as u]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Fixtures
@@ -46,6 +47,14 @@
   {"id"         "http://www.example.com/tincan/activities/multipart"
    "objectType" "Activity"})
 
+(def sample-attachment
+  {"usageType"   "http://example.com/attachment-usage/test"
+   "display"     {"en-US" "A test attachment"}
+   "description" {"en-US" "A test attachment (description)"}
+   "contentType" "text/plain"
+   "length"      27
+   "sha2"        "495395e777cd98da653df9615d09c0fd6bb2f8d4788394cd53c56a3bfdcd848a"})
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -54,17 +63,49 @@
   (let [lrs-authority     {"mbox"       "mailto:a@example.com"
                            "objectType" "Agent"}
         foreign-authority {"mbox"       "mailto:b@example.com"
-                           "objectType" "Agent"}]
+                           "objectType" "Agent"}
+        ;; Statements
+        statement-1 {"id"     sample-id
+                     "actor"  sample-group
+                     "verb"   sample-verb
+                     "object" sample-activity}
+        statement-2 {"id"        sample-id
+                     "actor"     sample-group
+                     "verb"      sample-verb
+                     "object"    sample-activity
+                     "authority" foreign-authority}
+        statement-3 {"id"          sample-id
+                     "actor"       sample-group
+                     "verb"        (assoc sample-verb "display" {})
+                     "object"      (assoc sample-activity
+                                          "definition"
+                                          {"name"        {}
+                                           "description" {}})
+                     "attachments" [(-> sample-attachment
+                                        (assoc "display" {})
+                                        (assoc "description" {}))]
+                     "context"     {}
+                     "result"      {}}]
+    (testing "adds timestamp, stored, version, and authority"
+      (let [statement* (su/prepare-statement lrs-authority statement-1)]
+        (is (inst? (u/str->time (get statement* "timestamp"))))
+        (is (inst? (u/str->time (get statement* "stored"))))
+        (is (= su/xapi-version (get statement* "version")))
+        (is (= lrs-authority (get statement* "authority")))))
     (testing "overwrites authority"
       (is (= lrs-authority
-             (-> (su/prepare-statement
-                  lrs-authority
-                  {"id"        sample-id
-                   "actor"     sample-group
-                   "verb"      sample-verb
-                   "object"    sample-activity
-                   "authority" foreign-authority})
-                 (get "authority")))))))
+             (-> (su/prepare-statement lrs-authority statement-2)
+                 (get "authority")))))
+    (testing "dissocs empty maps"
+      (is (= {"id"          sample-id
+              "actor"       sample-group
+              "verb"        sample-verb-dissoc
+              "object"      sample-activity-dissoc
+              "attachments" [(dissoc sample-attachment
+                                     "display"
+                                     "description")]}
+             (-> (su/prepare-statement lrs-authority statement-3)
+                 (dissoc "timestamp" "stored" "authority" "version")))))))
 
 (deftest statements-equal-test
   (testing "statement equality"
