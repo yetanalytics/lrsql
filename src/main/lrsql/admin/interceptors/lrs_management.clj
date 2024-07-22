6d4b22bbf0d3388cf02075a4b304b93355c0e76e
Daniel Bell <daniel@yetanalytics.com>
2024-07-22 14:42:33 -0500

    pr feedback

--- src/main/lrsql/admin/interceptors/lrs_management.clj
+++ src/main/lrsql/admin/interceptors/lrs_management.clj
@@ -4,8 +4,9 @@
             [io.pedestal.interceptor.chain :as chain]
             [lrsql.admin.protocol :as adp]
             [lrsql.spec.admin :as ads]
-            [lrsql.system.openapi :as openapi]
-            [com.yetanalytics.gen-openapi.core :as oa-core]))
+            [com.yetanalytics.gen-openapi.core :as oa-core]
+            [com.yetanalytics.gen-openapi.generate.schema :as gs]
+            [com.yetanalytics.lrs.pedestal.openapi :as lrs-oa]))
 
 (def validate-delete-actor-params
   (interceptor
@@ -34,12 +35,16 @@
                       :response {:status 200
                                  :body params})))}))
 
-(def openapi
+(defn openapi [routes]
   (interceptor
    {:name ::openapi
     :enter (fn openapi [ctx]
              (assoc ctx :response
                     {:status 200
                      :body (oa-core/make-oa-map
-                            openapi/general-map
-                            @openapi/oa-routes)}))}))
+                            {:openapi "3.0.0"
+                             :info {:title "LRSQL"
+                                    :version "0.7.2"}
+                             :externalDocs {:url "https://github.com/yetanalytics/lrsql/blob/main/doc/endpoints.md"}
+                             :components (gs/dsl lrs-oa/components)}
+                            routes)}))}))
