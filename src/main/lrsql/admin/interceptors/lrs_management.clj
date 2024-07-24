bfd0fe373d68df90be67fe55fd4cd6fd16c2812a
Daniel Bell <daniel@yetanalytics.com>
2024-07-24 16:41:30 -0500

    adds lrsql-specific parts to openapi spec

--- src/main/lrsql/admin/interceptors/lrs_management.clj
+++ src/main/lrsql/admin/interceptors/lrs_management.clj
@@ -35,6 +35,17 @@
                       :response {:status 200
                                  :body params})))}))
 
+(def add-lrsql-specifics [lrs-oa-spec]
+  (-> lrs-oa-spec
+      (update :schemas merge {:KeyPair (gs/o {:api-key :t#string
+                                              :secret-key :t#string})
+                              :ScopedKeyPair {:allOf [:r#KeyPair
+                                                      :r#Scopes]}
+                              :Scopes (gs/o {:scopes (gs/a :t#string)})})
+      (update :securitySchemes {:bearerAuth {:type :http
+                                            :scheme :bearer
+                                            :bearerFormat :JWT}})))
+
 (defn openapi [routes version]
   (interceptor
    {:name ::openapi
@@ -46,5 +57,6 @@
                              :info {:title "LRSQL"
                                     :version version}
                              :externalDocs {:url "https://github.com/yetanalytics/lrsql/blob/main/doc/endpoints.md"}
-                             :components (gs/dsl lrs-oa/components)}
+                             :components (gs/dsl (add-lrsql-specifics
+                                                  lrs-oa/components))}
                             routes)}))}))
