093c2e1e044021655d8e0b83e42dbba229b73b67
Daniel Bell <daniel@yetanalytics.com>
2024-07-25 16:40:07 -0500

    redid version system

--- src/main/lrsql/init/git_data.clj
+++ src/main/lrsql/init/git_data.clj
@@ -1,30 +1,10 @@
 (ns lrsql.init.git-data
-  (:require [cheshire.core :as json]
-            [clojure.string]))
-
-(defn read-git []
-  (let [read-text (or (try (slurp "target/bundle/git-details.json")
-                           (catch Exception _
-                             nil))
-                      (try (slurp "dev-resources/git-details.json")
-                           (catch Exception _
-                             nil)))]
-    (json/parse-string read-text)))
-
-(defn- to-version [git-data]
-  (let [{:strs [tag]} git-data]
-        (if (not= tag "")
-      tag
-      nil)))
+  (:require [clojure.edn :as edn]
+            [clojure.string]
+            [clojure.java.shell :refer [sh]]))
 
 (defn read-version []
-  (to-version (read-git)))
-
-(defn- to-last-version [git-data]
-  (or (to-version git-data)
-      (-> git-data (get "last_tag" "")
-          (clojure.string/split #"\-")
-          first)))
-
-(defn read-last-version []
-  (to-last-version (read-git)))
+  (try (edn/read-string (slurp "lrsql/config/git-details.edn"))
+       (catch Exception _
+         (try (clojure.string/trim (str "DEV-" (:out (sh "git" "describe" "--tags"))))
+              (catch Exception _ "DEV")))))
