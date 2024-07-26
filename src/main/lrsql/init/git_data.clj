68548de651903d9825bb2e6a94bf9f17ece5ba36
Daniel Bell <daniel@yetanalytics.com>
2024-07-26 11:00:31 -0500

    changed fallback for version read

--- src/main/lrsql/init/git_data.clj
+++ src/main/lrsql/init/git_data.clj
@@ -6,5 +6,11 @@
 (defn read-version []
   (try (edn/read-string (slurp "lrsql/config/git-details.edn"))
        (catch Exception _
-         (try (clojure.string/trim (str "DEV-" (:out (sh "git" "describe" "--tags"))))
+         (try
+           (let [hash (:out (sh "git" "rev-parse" "HEAD"))]
+             (->> hash
+                  (take 7)
+                  (apply str)
+                  (str "DEV-")
+                  clojure.string/trim))
               (catch Exception _ "DEV")))))
