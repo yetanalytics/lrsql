01674d0304ea7936ab4ddebcd9422048f151c8b3
kelvinqian00 <kelvinqian2@gmail.com>
2024-10-31 18:01:14 -0400

    JWT op specs

---
+++ src/main/lrsql/spec/admin/jwt.clj
@@ -0,0 +1,46 @@
+(ns lrsql.spec.admin.jwt
+  (:require [clojure.spec.alpha :as s]
+            [lrsql.backend.protocol :as bp]
+            [lrsql.spec.common :as c]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Interface
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn admin-jwt-backend?
+  [bk]
+  (satisfies? bp/JWTBlocklistBackend bk))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Inputs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(s/def ::account-id :lrsql.spec.admin/account-id)
+(s/def ::current-time c/instant-spec)
+(s/def ::expiration c/instant-spec)
+
+(def query-blocked-jwt-input-spec
+  (s/keys :req-un [::account-id
+                   ::current-time]))
+
+(def insert-blocked-jwt-input-spec
+  (s/keys :req-un [::account-id
+                   ::expiration]))
+
+(def delete-blocked-jwt-account-input-spec
+  (s/keys :req-un [::account-id]))
+
+(def delete-blocked-jwt-time-input-spec
+  (s/keys :req-un [::current-time]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Results
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(s/def :lrsql.spec.admin.jwt.command/result ::account-id)
+
+(def blocked-jwt-op-result-spec
+  (s/keys :req-un [:lrsql.spec.admin.jwt.command/result]))
+
+(def blocked-jwt-query-result-spec
+  boolean?)
