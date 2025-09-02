486718b3bdb2be042e74997f8649527a36a7aaef
Milton Reder <milt@yetanalytics.com>
2025-09-02 16:27:54 -0400

    singleton tc for postgres

--- src/test/lrsql/test_runner.clj
+++ src/test/lrsql/test_runner.clj
@@ -1,6 +1,7 @@
 (ns lrsql.test-runner
   (:require [cognitect.test-runner.api :as runner]
-            [lrsql.test-support :as support]))
+            [lrsql.test-support :as support]
+            [clj-test-containers.core :as tc]))
 
 (defn -main
   [& args]
@@ -9,4 +10,12 @@
                   (case db
                     "sqlite"   support/fresh-sqlite-fixture
                     "postgres" support/fresh-postgres-fixture)]
-      (runner/test {:dirs ["src/test"]}))))
+      (try
+        (binding [support/*postgres-container*
+                  (case db
+                    "sqlite" support/*postgres-container*
+                    "postgres" (tc/start! support/*postgres-container*))]
+          (runner/test {:dirs ["src/test"]}))
+        (finally
+          (when (= "postgres" db)
+            (tc/stop! support/*postgres-container*)))))))
