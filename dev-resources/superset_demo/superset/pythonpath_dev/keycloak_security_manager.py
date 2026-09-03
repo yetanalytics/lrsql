0b74bd69472639e2f187dc84a52d9341621ba180
Milton Reder <milt@yetanalytics.com>
2026-09-03 11:33:39 -0400

    pin working superset and remove old packaging

--- dev-resources/superset_demo/superset/pythonpath_dev/keycloak_security_manager.py
+++ dev-resources/superset_demo/superset/pythonpath_dev/keycloak_security_manager.py
@@ -1,55 +1,27 @@
-from flask_appbuilder.security.manager import AUTH_OID
+import logging
+import os
+
 from superset.security import SupersetSecurityManager
-from flask_oidc import OpenIDConnect
-from flask_appbuilder.security.views import AuthOIDView
-from flask_login import login_user
-from urllib.parse import quote
-from flask_appbuilder.views import expose
-from flask import request, redirect
 
 
-class OIDCSecurityManager(SupersetSecurityManager):
-
-    def __init__(self, appbuilder):
-        super(OIDCSecurityManager, self).__init__(appbuilder)
-        if self.auth_type == AUTH_OID:
-            self.oid = OpenIDConnect(self.appbuilder.get_app)
-        self.authoidview = AuthOIDCView
+logger = logging.getLogger(__name__)
 
 
-class AuthOIDCView(AuthOIDView):
+class KeycloakSecurityManager(SupersetSecurityManager):
+    def oauth_user_info(self, provider, response=None):
+        if provider != "keycloak":
+            return {}
 
-    @expose('/login/', methods=['GET', 'POST'])
-    def login(self, flag=True):
-        sm = self.appbuilder.sm
-        oidc = sm.oid
-        superset_roles = ["Admin", "Alpha", "Gamma", "Public", "granter", "sql_lab"]
-        default_role = "Gamma"
+        remote = self.appbuilder.sm.oauth_remotes[provider]
+        userinfo_response = remote.get(os.environ["OAUTH_USERINFO_URL"])
+        userinfo_response.raise_for_status()
+        userinfo = userinfo_response.json()
+        logger.debug("Received Keycloak user information for OAuth login")
 
-        @self.appbuilder.sm.oid.require_login
-        def handle_login():
-            user = sm.auth_user_oid(oidc.user_getfield('email'))
-
-            if user is None:
-                info = oidc.user_getinfo(['preferred_username', 'given_name', 'family_name', 'email', 'roles'])
-                roles = [role for role in superset_roles if role in info.get('roles', [])]
-                roles += [default_role, ] if not roles else []
-                user = sm.add_user(info.get('preferred_username'), info.get('given_name', ''), info.get('family_name', ''),
-                                   info.get('email'), [sm.find_role(role) for role in roles])
-
-            login_user(user, remember=False)
-            return redirect(self.appbuilder.get_url_for_index)
-
-        return handle_login()
-
-    @expose('/logout/', methods=['GET', 'POST'])
-    def logout(self):
-        oidc = self.appbuilder.sm.oid
-
-        oidc.logout()
-        super(AuthOIDCView, self).logout()
-        redirect_url = request.url_root.strip('/')
-        # redirect_url = request.url_root.strip('/') + self.appbuilder.get_url_for_login
-
-        return redirect(
-            oidc.client_secrets.get('issuer') + '/protocol/openid-connect/logout?redirect_uri=' + quote(redirect_url))
+        return {
+            "username": userinfo.get("preferred_username", ""),
+            "first_name": userinfo.get("given_name", ""),
+            "last_name": userinfo.get("family_name", ""),
+            "email": userinfo.get("email", ""),
+            "role_keys": userinfo.get("roles", []),
+        }
--- dev-resources/superset_demo/superset/pythonpath_dev/superset_config.py
+++ dev-resources/superset_demo/superset/pythonpath_dev/superset_config.py
@@ -1,47 +1,25 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-#
-# This file is included in the final Docker image and SHOULD be overridden when
-# deploying the image to prod. Settings configured here are intended for use in local
-# development environments. Also note that superset_config_docker.py is imported
-# as a final step as a means to override "defaults" configured here
-#
-from flask_appbuilder.security.manager import AUTH_OID
-from keycloak_security_manager import OIDCSecurityManager
 import logging
 import os
-from typing import Optional
-from cachelib.file import FileSystemCache
+
 from celery.schedules import crontab
+from flask_appbuilder.security.manager import AUTH_DB, AUTH_OAUTH
+from flask_caching.backends.filesystemcache import FileSystemCache
 
-logger = logging.getLogger()
+from keycloak_security_manager import KeycloakSecurityManager
 
-def get_env_variable(var_name: str, default: Optional[str] = None) -> str:
-    """Get the environment variable or raise exception."""
-    try:
-        return os.environ[var_name]
-    except KeyError:
-        if default is not None:
-            return default
-        else:
-            error_msg = "The environment variable {} was missing, abort...".format(
-                var_name
-            )
-            raise EnvironmentError(error_msg)
+
+def get_env_variable(name: str, default: str | None = None) -> str:
+    value = os.getenv(name, default)
+    if value is None:
+        raise RuntimeError(f"Required environment variable {name} is not set")
+    return value
+
+
+def get_env_boolean(name: str, default: bool = False) -> bool:
+    value = os.getenv(name)
+    if value is None:
+        return default
+    return value.lower() in {"1", "true", "yes", "on"}
 
 
 DATABASE_DIALECT = get_env_variable("DATABASE_DIALECT")
@@ -51,32 +29,41 @@ DATABASE_HOST = get_env_variable("DATABASE_HOST")
 DATABASE_PORT = get_env_variable("DATABASE_PORT")
 DATABASE_DB = get_env_variable("DATABASE_DB")
 
-# The SQLAlchemy connection string.
-SQLALCHEMY_DATABASE_URI = "%s://%s:%s@%s:%s/%s" % (
-    DATABASE_DIALECT,
-    DATABASE_USER,
-    DATABASE_PASSWORD,
-    DATABASE_HOST,
-    DATABASE_PORT,
-    DATABASE_DB,
+SQLALCHEMY_DATABASE_URI = (
+    f"{DATABASE_DIALECT}://{DATABASE_USER}:{DATABASE_PASSWORD}"
+    f"@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_DB}"
 )
 
-REDIS_HOST = get_env_variable("REDIS_HOST")
-REDIS_PORT = get_env_variable("REDIS_PORT")
+REDIS_HOST = get_env_variable("REDIS_HOST", "redis")
+REDIS_PORT = get_env_variable("REDIS_PORT", "6379")
 REDIS_CELERY_DB = get_env_variable("REDIS_CELERY_DB", "0")
 REDIS_RESULTS_DB = get_env_variable("REDIS_RESULTS_DB", "1")
 
 RESULTS_BACKEND = FileSystemCache("/app/superset_home/sqllab")
+CACHE_CONFIG = {
+    "CACHE_TYPE": "RedisCache",
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "CACHE_KEY_PREFIX": "superset_",
+    "CACHE_REDIS_HOST": REDIS_HOST,
+    "CACHE_REDIS_PORT": REDIS_PORT,
+    "CACHE_REDIS_DB": REDIS_RESULTS_DB,
+}
+DATA_CACHE_CONFIG = CACHE_CONFIG
+THUMBNAIL_CACHE_CONFIG = CACHE_CONFIG
 
 
-class CeleryConfig(object):
-    BROKER_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_CELERY_DB}"
-    CELERY_IMPORTS = ("superset.sql_lab",)
-    CELERY_RESULT_BACKEND = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_RESULTS_DB}"
-    CELERYD_LOG_LEVEL = "DEBUG"
-    CELERYD_PREFETCH_MULTIPLIER = 1
-    CELERY_ACKS_LATE = False
-    CELERYBEAT_SCHEDULE = {
+class CeleryConfig:
+    broker_url = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_CELERY_DB}"
+    imports = (
+        "superset.sql_lab",
+        "superset.tasks.scheduler",
+        "superset.tasks.thumbnails",
+        "superset.tasks.cache",
+    )
+    result_backend = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_RESULTS_DB}"
+    worker_prefetch_multiplier = 1
+    task_acks_late = False
+    beat_schedule = {
         "reports.scheduler": {
             "task": "reports.scheduler",
             "schedule": crontab(minute="*", hour="*"),
@@ -89,72 +76,58 @@ class CeleryConfig(object):
 
 
 CELERY_CONFIG = CeleryConfig
+SECRET_KEY = get_env_variable("SECRET_KEY")
+BABEL_DEFAULT_LOCALE = get_env_variable("BABEL_DEFAULT_LOCALE", "en")
+LANGUAGES = {
+    "en": {"flag": "us", "name": "English"},
+    # Browsers commonly send en-US, which Flask-Babel normalizes to en_US.
+    # Superset 6.1's navbar expects every negotiated locale in this mapping.
+    "en_US": {"flag": "us", "name": "English"},
+}
+PUBLIC_ROLE_LIKE = get_env_variable("PUBLIC_ROLE_LIKE", "Gamma")
+SQLLAB_CTAS_NO_LIMIT = True
+RATELIMIT_STORAGE_URI = f"redis://{REDIS_HOST}:{REDIS_PORT}/2"
 
-FEATURE_FLAGS = {"ALERT_REPORTS": True}
+FEATURE_FLAGS = {
+    "ALERT_REPORTS": True,
+    "EMBEDDED_SUPERSET": get_env_boolean("SUPERSET_FEATURE_EMBEDDED_SUPERSET"),
+}
 ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
 WEBDRIVER_BASEURL = "http://superset:8088/"
-# The base URL for the email report hyperlinks.
-WEBDRIVER_BASEURL_USER_FRIENDLY = WEBDRIVER_BASEURL
+WEBDRIVER_BASEURL_USER_FRIENDLY = "http://localhost:8088/"
 
-SQLLAB_CTAS_NO_LIMIT = True
+log_level_text = get_env_variable("SUPERSET_LOG_LEVEL", "INFO")
+LOG_LEVEL = getattr(logging, log_level_text.upper(), logging.INFO)
 
-#
-# Optionally import superset_config_docker.py (which will have been included on
-# the PYTHONPATH) in order to allow for local settings to be overridden
-#
-try:
-    import superset_config_docker
-    from superset_config_docker import *  # noqa
-
-    logger.info(
-        f"Loaded your Docker configuration at " f"[{superset_config_docker.__file__}]"
+SUPERSET_AUTH_MODE = get_env_variable("SUPERSET_AUTH_MODE", "oauth").lower()
+if SUPERSET_AUTH_MODE == "db":
+    AUTH_TYPE = AUTH_DB
+elif SUPERSET_AUTH_MODE == "oauth":
+    AUTH_TYPE = AUTH_OAUTH
+    OAUTH_PROVIDERS = [
+        {
+            "name": "keycloak",
+            "token_key": "access_token",
+            "icon": "fa-key",
+            "remote_app": {
+                "client_id": get_env_variable("OAUTH_CLIENT_ID"),
+                "client_secret": get_env_variable("OAUTH_CLIENT_SECRET"),
+                "client_kwargs": {"scope": "openid profile email"},
+                "server_metadata_url": get_env_variable("OAUTH_METADATA_URL"),
+            },
+        }
+    ]
+    CUSTOM_SECURITY_MANAGER = KeycloakSecurityManager
+    AUTH_USER_REGISTRATION = True
+    AUTH_USER_REGISTRATION_ROLE = "Gamma"
+    AUTH_ROLES_SYNC_AT_LOGIN = True
+    AUTH_ROLES_MAPPING = {
+        "Admin": ["Admin"],
+        "Alpha": ["Alpha"],
+        "Gamma": ["Gamma"],
+    }
+else:
+    raise RuntimeError(
+        "SUPERSET_AUTH_MODE must be either 'db' or 'oauth', "
+        f"not {SUPERSET_AUTH_MODE!r}"
     )
-except ImportError:
-    logger.info("Using default Docker config...")
-
-
-# Personalized configurations
-
-SECRET_KEY = get_env_variable("SECRET_KEY")
-
-# ---------------------------------------------------
-# Babel config for translations
-# ---------------------------------------------------
-# Setup default language
-# BABEL_DEFAULT_LOCALE = get_env_variable('BABEL_DEFAULT_LOCALE')
-# # Your application default translation path
-# BABEL_DEFAULT_FOLDER = "superset/translations"
-# # The allowed translation for you app
-# LANGUAGES = {
-#     "en": {"flag": "us", "name": "English"},
-#     "fr": {"flag": "fr", "name": "French"},
-#     "pt_BR": {"flag": "br", "name": "Brazilian Portuguese"},
-#     # "es": {"flag": "es", "name": "Spanish"},
-#     # "it": {"flag": "it", "name": "Italian"},
-#     # "zh": {"flag": "cn", "name": "Chinese"},
-#     # "ja": {"flag": "jp", "name": "Japanese"},
-#     # "de": {"flag": "de", "name": "German"},
-#     # "pt": {"flag": "pt", "name": "Portuguese"},
-#     # "ru": {"flag": "ru", "name": "Russian"},
-#     # "ko": {"flag": "kr", "name": "Korean"},
-#     # "sl": {"flag": "si", "name": "Slovenian"},
-# }
-
-PUBLIC_ROLE_LIKE = get_env_variable('PUBLIC_ROLE_LIKE')
-
-# OIDC config
-'''
----------------------------KEYCLOACK ----------------------------
-'''
-curr = os.path.abspath(os.getcwd())
-AUTH_TYPE = AUTH_OID
-OIDC_CLIENT_SECRETS = curr + '/docker/pythonpath_dev/client_secret.json'
-OIDC_ID_TOKEN_COOKIE_SECURE = False
-OIDC_OPENID_REALM = get_env_variable('OIDC_OPENID_REALM')
-OIDC_INTROSPECTION_AUTH_METHOD = 'client_secret_post'
-CUSTOM_SECURITY_MANAGER = OIDCSecurityManager
-AUTH_USER_REGISTRATION = True
-AUTH_USER_REGISTRATION_ROLE = 'Gamma'
-'''
---------------------------------------------------------------
-'''
